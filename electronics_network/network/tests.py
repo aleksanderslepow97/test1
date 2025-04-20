8e81bb17be9a78f672739410812e119116d3ee3a
Aleksander <sanya.slepow2015@yandex.ru>
2025-04-20 19:47:33 +0500

    commit3

--- electronics_network/network/tests.py
+++ electronics_network/network/tests.py
@@ -1,6 +1,4 @@
-# from django.test import TestCase
-
-# Create your tests here.
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.utils import timezone
 
@@ -71,3 +69,72 @@ class ProductModelTest(TestCase):
     def test_product_str_method(self):
         """Проверка метода __str__ модели Product."""
         self.assertEqual(str(self.product), 'Test Product')
+
+
+class AdminNodeTests(TestCase):
+    """Тесты для административного интерфейса модели Node."""
+
+    def setUp(self):
+        """Создание тестового пользователя и тестовых объектов."""
+        self.user = get_user_model().objects.create_superuser(
+            username='admin',
+            password='password'
+        )
+        self.node1 = Node.objects.create(name='Node 1', city='City A', supplier='Supplier A', debt=100)
+        self.node2 = Node.objects.create(name='Node 2', city='City A', supplier='Supplier B', debt=200)
+
+    def test_list_display(self):
+        """Проверка правильного отображения списка объектов Node."""
+        self.client.login(username='admin', password='password')
+        response = self.client.get('/admin/app_name/node/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.node1.name)
+        self.assertContains(response, self.node2.name)
+        self.assertContains(response, self.node1.debt)
+        self.assertContains(response, self.node2.debt)
+
+    def test_clear_debt_action(self):
+        """Проверка выполнения действия очистки задолженности."""
+        self.client.login(username='admin', password='password')
+
+        # Убедимся, что у узлов есть задолженность перед выполнением действия
+        self.assertEqual(self.node1.debt, 100)
+        self.assertEqual(self.node2.debt, 200)
+
+        response = self.client.post(
+            '/admin/app_name/node/action/',
+            {'post': 'clear_debt', '_selected_action': [self.node1.id, self.node2.id]}
+        )
+
+        # Проверка, что задолженность очищена
+        self.node1.refresh_from_db()
+        self.node2.refresh_from_db()
+        self.assertEqual(self.node1.debt, 0)
+        self.assertEqual(self.node2.debt, 0)
+        self.assertContains(response, "Задолженность успешно очищена")
+
+
+class AdminProductTests(TestCase):
+    """Тесты для административного интерфейса модели Product."""
+
+    def setUp(self):
+        """Создание тестового пользователя и тестовых объектов."""
+        self.user = get_user_model().objects.create_superuser(
+            username='admin',
+            password='password'
+        )
+        self.node = Node.objects.create(name='Node 1', city='City A', supplier='Supplier A', debt=100)
+        self.product = Product.objects.create(name='Product 1', model='Model 1', release_date='2023-01-01',
+                                              node=self.node)
+
+    def test_product_list_display(self):
+        """Проверка правильного отображения списка объектов Product."""
+        self.client.login(username='admin', password='password')
+        response = self.client.get('/admin/app_name/product/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.product.name)
+        self.assertContains(response, self.product.model)
+        self.assertContains(response, self.product.release_date)
+        self.assertContains(response, self.product.node.name)
--- electronics_network/network/views.py
+++ electronics_network/network/views.py
@@ -7,12 +7,45 @@ from .serializers import NodeSerializer
 
 
 class NodeViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet для модели узла (Node).
+
+    Этот ViewSet предоставляет полный набор представлений для
+    модели Node, включая создание, чтение, обновление и удаление
+    узлов. В методе обновления запрещено изменение поля 'debt',
+    если оно присутствует в запросе.
+
+    Атрибуты:
+        queryset: Все экземпляры модели Node.
+        serializer_class: Сериализатор, используемый для обработки
+                          экземпляров Node.
+
+    Методы:
+        update: Переопределяет стандартный метод обновления, чтобы
+                исключить обновление поля 'debt'.
+    """
+
     queryset = Node.objects.all()
     serializer_class = NodeSerializer
 
     def update(self, request, *args, **kwargs):
+        """
+        Переопределенный метод обновления.
+
+        Запрещает обновление поля 'debt' в запросе,
+        если оно присутствует в данных запроса.
+        Удаляет 'debt' из данных запроса перед вызовом
+        стандартного метода обновления.
+
+        Аргументы:
+            request: Объект запроса, содержащий данные для обновления.
+            *args: Дополнительные аргументы.
+            **kwargs: Ключевые аргументы.
+
+        Возвращает:
+            Response: Ответ API после обработки запроса обновления.
+        """
         instance = self.get_object()
-        # Запрет на обновление задолженности
         debt = request.data.get('debt', None)
         if debt is not None:
             request.data.pop('debt')
@@ -20,12 +53,38 @@ class NodeViewSet(viewsets.ModelViewSet):
 
 
 class NobeViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet для модели узла (Node) с ограничениями доступа.
+
+    Этот ViewSet предоставляет полный набор представлений для
+    модели Node и требует аутентификации для выполнения действий.
+    Включает возможность фильтрации узлов по стране.
+
+    Атрибуты:
+        queryset: Все экземпляры модели Node.
+        serializer_class: Сериализатор, используемый для обработки
+                          экземпляров Node.
+        permission_classes: Список классов разрешений для контроля доступа.
+
+    Методы:
+        get_queryset: Переопределяет метод получения запроса для
+                      фильтрации узлов по стране.
+    """
+
     queryset = Node.objects.all()
     serializer_class = NodeSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        # Пример фильтрации по стране
+        """
+        Переопределенный метод получения запроса.
+
+        Фильтрует queryset узлов по стране, если параметр 'country'
+        указан в запросе. Возвращает отфильтрованный queryset.
+
+        Возвращает:
+            QuerySet: Отфильтрованный набор объектов Node.
+        """
         queryset = super().get_queryset()
         country = self.request.query_params.get('country', None)
         if country:
