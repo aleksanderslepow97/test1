9da4517b8c27fd4e58e65b92b5a29097905e2906
Aleksander <sanya.slepow2015@yandex.ru>
2025-04-20 20:02:43 +0500

    commit4

--- electronics_network/electronics_network/settings.py
+++ electronics_network/electronics_network/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'rest_framework',
     'network',
-    'django.contrib.admin',  # Если требуется для админки
+    # 'django.contrib.admin',  # Если требуется для админки
 ]
 
 MIDDLEWARE = [
